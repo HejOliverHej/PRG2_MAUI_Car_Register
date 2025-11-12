7dc87e26df27298bac2639ef477d28413493cbda
HEjOliverHej <oliver.bafverholm@elev.ga.lbs.se>
2025-11-12 09:18:30 +0100

    Gjort en Baseviewmodel med INotifyPropertyChanged

--- PRG_MAUI_Car_Register/ViewModel/BaseViewModel.cs
+++ PRG_MAUI_Car_Register/ViewModel/BaseViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PRG_MAUI_Car_Register.ViewModel
 {
-    internal class BaseViewModel
+    internal class BaseViewModel 
     {
     }
 }
